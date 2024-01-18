e70bfd8f1c8fe1b542ca881ee73a1624b38d9c35
zhouzhou <76592626+zhouzhouchn@users.noreply.github.com>
2024-01-19 01:22:55 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,13 +2,13 @@ export const SITE = {
   title: '潮流周刊',
   author: 'JasonChou',
   description: '记录JasonChou不枯燥的生活，欢迎关注',
-  keywords: 'Blog,前端,macOS,IOS,Quantumult X,Swift',
+  keywords: 'Blog,后端,macOS,IOS,Quantumult X,Swift',
   icon: 'https://gw.alipayobjects.com/zos/k/qv/coffee-2-icon.png',
   pic: 'https://cdn.fliggy.com/upic/2XFNGI.png',
   homePage: 'https://jasonchou.vercel.app',
   blogPage: 'https://jasonchou.vercel.app/',
   twitterId: 'JasonChou_X',
   githubId: 'zhouzhouchn',
-  repo: 'tw93/weekly'
+  repo: 'zhouzhouchn/weekly-tw93'
 };
 
