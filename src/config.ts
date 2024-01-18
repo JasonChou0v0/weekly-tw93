85ae3f7478fdae11c390952774687e953ff37453
zhouzhou <76592626+zhouzhouchn@users.noreply.github.com>
2024-01-19 00:57:00 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,14 @@
 export const SITE = {
   title: '潮流周刊',
-  author: 'Tw93',
-  description: '记录工程师 Tw93 的不枯燥生活，每周一发布，欢迎关注',
-  keywords: 'Tw93,Weekly,Blog,前端,macOS,MiaoYan,Pack,Swift',
+  author: 'JasonChou',
+  description: '记录JasonChou不枯燥的生活，欢迎关注',
+  keywords: 'Blog,前端,macOS,IOS,Quantumult X,Swift',
   icon: 'https://gw.alipayobjects.com/zos/k/qv/coffee-2-icon.png',
   pic: 'https://cdn.fliggy.com/upic/2XFNGI.png',
-  homePage: 'https://weekly.tw93.fun',
-  blogPage: 'https://tw93.fun/',
-  twitterId: 'HiTw93',
-  githubId: 'tw93',
+  homePage: 'https://jasonchou.vercel.app',
+  blogPage: 'https://jasonchou.vercel.app/',
+  twitterId: 'JasonChou_X',
+  githubId: 'zhouzhouchn',
   repo: 'tw93/weekly'
 };
 
