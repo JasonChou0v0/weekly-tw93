9ebbdd917ed5ba8a5788e3f87ab8a02b1ccc6d5f
zhouzhou <76592626+zhouzhouchn@users.noreply.github.com>
2024-01-20 00:01:14 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const SITE = {
-  title: 'Jason の 碎碎念',
+  title: 'Jason 碎碎念',
   author: 'JasonChou',
   description: '记录JasonChou不枯燥的生活，欢迎关注',
   keywords: 'Blog,后端,macOS,IOS,Quantumult X,Swift',
