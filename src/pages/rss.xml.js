5aa9d196411cf3054aecf332e8d2c92df94dd25b
JasonChou <76592626+JasonChou0v0@users.noreply.github.com>
2024-01-22 17:00:52 +0800

    Update rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -14,9 +14,9 @@ posts.splice(10);
 
 export const get = () =>
   rss({
-    title: '潮流周刊',
+    title: 'Jason 碎碎念',
     description: '记录 JasonChou 的日常生活',
-    site: 'https://weekly.tw93.fun/',
+    site: 'https://jasonchou.vercel.app/',
     customData: `<image><url>https://gw.alipayobjects.com/zos/k/qv/coffee-2-icon.png</url></image>`,
     items: posts.map((item) => {
       const url = item.url;
